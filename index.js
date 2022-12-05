e497123edeeab921b5a26e5c0811d08059c3e1bd
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2022-12-05 21:54:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     });
+
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        }),
+        
+   
+    })
+
+    .then((response) => {return response.json();
+    })
+    .then((object) => {
+        document.body.innerHTML = object["id"];
+    })
+    .catch((error) => {
+        document.body.innerHTML = error.message;
+    });
+        
+
+
+}
